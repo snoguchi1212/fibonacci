767627a844309903cad6fca58a82c2792ef77b71
snoguchi <snoguchi.nbsr@gmail.com>
2022-12-13 01:56:18 +0900

    テストコード追加

--- app/server/main.py
+++ app/server/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, status
 
-from app.fibonacci import FibonacciNumber
+from app.model.fibonacci import FibonacciNumber
 
 app = FastAPI()
 
 
-@app.get("/fib", status_code=200)
+@app.get("/fib", status_code=status.HTTP_200_OK)
 def respond_nth_fibonacci_number(
         fibonacci_idx: int = Query(alias="n")
 ):
