25f9a3f4f7720e29c2b0856013029b5dd360122d
snoguchi <snoguchi.nbsr@gmail.com>
2022-12-13 10:27:28 +0900

    不備修正

--- tests/test_server/test_main.py
+++ tests/test_server/test_main.py
@@ -15,16 +15,32 @@ def test_incorrect_url():
                                  'msg': 'field required',
                                  'type': 'value_error.missing'}]}
 
+def test_index_returns_correct_1():
+    response = client.get("/fib?n=1")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"result": 1}
+
 
 def test_index_returns_correct_2():
     response = client.get("/fib?n=2")
 
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == 1
+    assert response.json() == {"result": 1}
 
 
 def test_index_returns_correct_99():
     response = client.get("/fib?n=99")
 
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == 218922995834555169026
+    assert response.json() == {"result": 218922995834555169026}
+
+
+def test_index_returns_over_n_10001():
+    response = client.get("/fib?n=10001")
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.json() == {'detail': [{'ctx': {'limit_value': 10000},
+                               'loc': ['query', 'n'],
+                               'msg': 'ensure this value is less than or equal to 10000',
+                               'type': 'value_error.number.not_le'}]}
