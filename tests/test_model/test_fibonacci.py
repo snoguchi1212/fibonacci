464c0247d45ad02dc01f6954e362d40eb93dad99
snoguchi <snoguchi.nbsr@gmail.com>
2022-12-13 09:09:38 +0900

    テストコード修正

--- tests/test_model/test_fibonacci.py
+++ tests/test_model/test_fibonacci.py
@@ -4,11 +4,11 @@ from app.model.fibonacci import FibonacciNumber
 
 class TestFibonacci:
 
-    def fibonacci_1st_number(self):
+    def test_fibonacci_1st_number(self):
         assert FibonacciNumber(2).fibonacci_nth_number() == 1
 
-    def fibonacci_2nd_number(self):
+    def test_fibonacci_2nd_number(self):
         assert FibonacciNumber(2).fibonacci_nth_number() == 1
 
-    def fibonacci_99th_number(self):
+    def test_fibonacci_99th_number(self):
         assert FibonacciNumber(99).fibonacci_nth_number() == 218922995834555169026
